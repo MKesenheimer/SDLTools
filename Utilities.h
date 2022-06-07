4ef7d120830043ba50e894fcf861a25cd4c4d202
MKesenheimer <mkesenheimer@gmx.net>
2022-06-07 21:25:05 +0200

    Added routine to render text

--- Utilities.h
+++ Utilities.h
@@ -185,5 +185,34 @@ namespace sdl::auxiliary {
             SDL_QueryTexture(tex, NULL, NULL, &w, &h);
             renderTexture(tex, ren, x, y, w, h);
         }
+
+        // Render text (without alpha blending)
+        // be sure to call TTF_Init() before using this function
+        inline static void renderText(const std::string &message, const std::string &fontFile, SDL_Color color, int fontSize, SDL_Renderer *renderer, int x, int y) {
+	        TTF_Font *font = TTF_OpenFont(fontFile.c_str(), fontSize);
+            if(font == NULL) {
+                std::cout << "renderText: Could not load font" << std::endl;
+            }
+	        renderText(message, font, color, renderer, x, y);
+	        TTF_CloseFont(font);
+        }
+
+        inline static void renderText(const std::string &message, TTF_Font* font, SDL_Color color, SDL_Renderer *renderer, int x, int y) {
+	        SDL_Surface *surf = TTF_RenderUTF8_Blended(font, message.c_str(), color);
+            if(surf == NULL) {
+                std::cout << "renderText:: Could not create surface." << std::endl;
+            }
+
+	        SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surf);
+            if(texture == NULL) {
+                std::cout << "renderText: Could not create texture." << std::endl;
+            }
+
+            renderTexture(texture, renderer, x, y);
+
+	        // Clean up the surface and font
+	        SDL_FreeSurface(surf);
+            SDL_DestroyTexture(texture);
+        }
     };
 }
