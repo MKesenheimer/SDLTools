a6b8e205549130ef516eee4a97fb62e3410258ed
MKesenheimer <mkesenheimer@gmx.net>
2022-06-17 14:51:24 +0200

    Extended CommandLineTools

--- CommandLineParser.h
+++ CommandLineParser.h
@@ -13,7 +13,7 @@ namespace sdl::auxiliary {
     {
     public:
         // reads the string after the identifier option, i.e. for arg** = -i path "path" is returned if option = "-i"
-        static inline char* getCmdOption(char** begin, char ** end, const std::string& option) {
+        static inline char* getCmdOption(char** begin, char** end, const std::string& option) {
             char** itr = std::find(begin, end, option);
             if (itr != end && ++itr != end) {
                 return *itr;
@@ -26,7 +26,7 @@ namespace sdl::auxiliary {
             return std::find(begin, end, option) != end;
         }
 
-        static inline std::string readCmdOption(char** begin, char ** end, const std::string& option) {
+        static inline std::string readCmdOption(char** begin, char** end, const std::string& option) {
             std::string tmp = std::string();
             if (cmdOptionExists(begin, end, option)) {
                 tmp = getCmdOption(begin, end, option);
@@ -34,32 +34,51 @@ namespace sdl::auxiliary {
             return tmp;
         }
 
-        static inline std::string readCmdNormalized(char** begin, char ** end, const std::string& option) {
+        static inline std::string readCmdNormalized(char** begin, char** end, const std::string& option) {
             std::string tmp = readCmdOption(begin, end, option);
             normalizePath(tmp);
             return tmp;
         }
 
-        static inline int readCmdInt(char** begin, char ** end, const std::string& option, int lower = 0, int upper = 0) {
+        static inline int readCmdInt(char** begin, char** end, const std::string& option, int lower = 0, int upper = 0) {
             if (!cmdOptionExists(begin, end, option))
                 return 0;
             
             std::string tmp = readCmdOption(begin, end, option);
-            int tmp2 = 0;
-            try { 
-                tmp2 = std::stoi(tmp);
-                if (lower != 0 || upper != 0) {
-                    if (tmp2 < lower || tmp2 > upper) {
-                        std::cout << "Error: Number not in range: " << tmp2 << ", (" << lower << ", " << upper << ")" << std::endl;
-                        return Utilities::constrain<int>(tmp2, lower, upper);
-                    }
-                }
-            } catch (const std::exception& e) {
-                std::cout << "Warning: " << e.what() << ": No input number: " << tmp << std::endl;
-            }
+            return strToInt(tmp, lower, upper);
+        }
 
+        // read in list of values: -option 1,2,3,4
+        static inline std::vector<std::string> readCmdList(char** begin, char** end, const std::string& option, const std::string& delimiter = ",") {
+            if (!cmdOptionExists(begin, end, option))
+                return std::vector<std::string>();
+
+            std::string s = readCmdOption(begin, end, option);
+            std::vector<std::string> tmp2;
+            size_t pos = 0;
+            std::string token;
+            while ((pos = s.find(delimiter)) != std::string::npos) {
+                token = s.substr(0, pos);
+                tmp2.push_back(token);
+                s.erase(0, pos + delimiter.length());
+            }
+            tmp2.push_back(s);
             return tmp2;
         }
+
+        static inline std::vector<int> readCmdListInt(char** begin, char** end, const std::string& option, int lower = 0, int upper = 0, const std::string& delimiter = ",") {
+            if (!cmdOptionExists(begin, end, option))
+                return std::vector<int>();
+
+            std::vector<std::string> s = readCmdList(begin, end, option, delimiter);
+            std::vector<int> tmp;
+            for (const std::string& str : s) {
+                tmp.push_back(strToInt(str, lower, upper));
+            }
+            return tmp;
+        }
+
+
 
         // replace \ with / and // with /
         static inline void normalizePath(std::string& path) {
@@ -80,6 +99,23 @@ namespace sdl::auxiliary {
                 path = start + rest;
                 pos = path.find("//");
             }
+        }
+
+    private:
+        static inline int strToInt(const std::string& str, int lower = 0, int upper = 0) {
+            int tmp = 0;
+            try { 
+                tmp = std::stoi(str);
+                if (lower != 0 || upper != 0) {
+                    if (tmp < lower || tmp > upper) {
+                        std::cout << "Warning: Number not in range: " << tmp << ", (" << lower << ", " << upper << ")" << std::endl;
+                        return Utilities::constrain<int>(tmp, lower, upper);
+                    }
+                }
+            } catch (const std::exception& e) {
+                std::cout << "Warning: " << e.what() << ": No input number: " << tmp << std::endl;
+            }
+            return tmp;
         }
     };
 }
